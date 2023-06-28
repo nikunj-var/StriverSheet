0dafcfbabdf318458d8361ec87aaedbed6c6cfa2
nikunj-var <nikunjvar2002@gmail.com>
2023-06-28 23:26:11 +0530

    changes done

--- BinarySearch/1Darrays/peakelement.cpp
+++ BinarySearch/1Darrays/peakelement.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int findPeakElement(vector<int>& nums) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start < end){
-            int mid=start+(end-start)/2;
-            if(nums[mid] < nums[mid+1]){
-                start = mid+1;
+    int findPeakElement(vector<int> &nums)
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1])
+            {
+                start = mid + 1;
             }
-            else{
-                end=mid;
+            else
+            {
+                end = mid;
             }
         }
         return start;
