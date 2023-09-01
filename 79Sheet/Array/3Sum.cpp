f5fc8b1ab27ea4cf0c150cacb819fe3a831133c0
Nikunj <nikunjvar2002@gmail.com>
2023-09-02 00:00:52 +0530

    added array question of 79sheet'

---
+++ 79Sheet/Array/3Sum.cpp
@@ -0,0 +1,94 @@
+/*
+
+https://leetcode.com/problems/3sum/description/
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i != 0 && nums[i - 1] == nums[i])
+            {
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j - 1] == nums[j])
+                    {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1])
+                    {
+                        k--;
+                    }
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
---
+++ DynamicProgramming/Dp_on_Stocks/besttimetobuy&sellstock.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int profit = 0;
+        int miniprice = prices[0];
+        for (int i = 1; i < prices.size(); i++)
+        {
+            int currprofit = prices[i] - miniprice;
+            profit = max(profit, currprofit);
+            miniprice = min(miniprice, prices[i]);
+        }
+        return profit;
+    }
+};
